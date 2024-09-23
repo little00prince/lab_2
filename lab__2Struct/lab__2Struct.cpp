9affa6d295cd0623c337197b0daa4d5d4eaf7aea
«Anton» <antonaryy98@gmail.com>
2024-09-23 19:05:02 +0700

    создал структуру

--- lab__2Struct/lab__2Struct.cpp
+++ lab__2Struct/lab__2Struct.cpp
@@ -1,5 +1,28 @@
 ﻿#include <iostream>
 
+struct Figures
+{
+    float area;
+    float volume;
+    int type;
+    union {
+        struct {
+            float a;
+            float diagonal;
+        }cube;
+        struct {
+            float r;
+            float d;
+        }ball;
+        struct {
+            float b;
+            int view;
+        }tetrapyramid;
+    }figure;
+};
+
+Figures first, * second = NULL;
+
 int main()
 {
 
