649a0b3ba05b987218d5cf644bc06f7b92407ea7
«Anton» <antonaryy98@gmail.com>
2024-09-23 19:17:47 +0700

    реализовал защиту на целые и дробные

--- lab__2Struct/lab__2Struct.cpp
+++ lab__2Struct/lab__2Struct.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 
 struct Figures
 {
@@ -25,5 +26,49 @@ Figures first, * second = NULL;
 
 int main()
 {
+    setlocale(LC_ALL, "Rus");
 
 }
+
+//Защита на целые числа
+int block_i(int min, int max)
+{
+    int type, f = 1;
+    do {
+        if (f != 1)
+        {
+            printf("Такого варианта нет. Попробуйте ещё раз: ");
+        }
+        while (scanf("%d", &type) != 1)
+        {
+            while (getchar() != '\n');
+            printf("Ошибка. Попробуйте ещё раз: ");
+        }
+        f = 0;
+        while (getchar() != '\n');
+    } while (type<min || type>max);
+    printf("\n");
+    return type;
+}
+//Защита на дробные числа
+float block_f()
+{
+    int f = 1;
+    float e;
+    do {
+        if (f != 1)
+        {
+            printf("Такого варианта нет. Попробуйте ещё раз: ");
+        }
+        while (scanf("%f", &e) != 1)
+        {
+            while (getchar() != '\n');
+            printf("Ошибка. Попробуйте ещё раз: ");
+        }
+        f = 0;
+        while (getchar() != '\n');
+    } while (e <= 0);
+    printf("\n");
+    return e;
+}
+
